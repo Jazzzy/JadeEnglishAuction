e332ebd32d692bf4f0b0897e418916a5a82b6919
Rubén Osorio López <gladislacebrafeliz@gmail.com>
2016-04-27 17:18:34 +0200

    Structure to projects

--- Seller/src/viewController/Main.java
+++ Seller/src/viewController/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package viewController;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("principal.fxml"));
+        primaryStage.setTitle("Seller");
+        primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
     }
 
